82b34fbce5868b1ec4eadedb52d45a3ee299fc7f
Jaroslav Pochobradský <jaroslav.pochobradsk@educhem.cz>
2023-04-18 12:12:28 +0200

    Přidejte soubory projektu.

---
+++ OOP/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Objekt
+    {
+        public int objemObjektu(int strana1,int strana2,int strana3)
+        {
+            int objemTelesa = strana1 * strana2 * strana3;
+            return objemTelesa;
+        }
+        public int povrchObjektu(int strana1, int strana2, int strana3)
+        {
+            int povrchTelesa = 2 * (strana1 * strana2 + strana2 * strana3 + strana1 * strana3);
+            return povrchTelesa;
+        }
+        public int telesovaUhlopricka(int strana1, int strana2, int strana3)
+        {
+            int vysledek = (int)Math.Sqrt(strana1 * strana1 + strana2 * strana2 + strana3 * strana3);
+            return vysledek;
+        }
+        public int stenovaUhlopricka(int strana1, int strana2)
+        {
+            int vysledek = (int)Math.Sqrt(strana1 * strana1 + strana2 * strana2);
+            return vysledek;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using OOP;
+Console.WriteLine("chceš vypočítat: objem, povrch, telesovou uhlopricku nebo stenovou uhlopricku?");
+string odpoved = Console.ReadLine();
+if (odpoved == "objem")
+{
+    Objekt objekt = new Objekt();
+    Console.WriteLine("Zadej délky stran");
+    int cislo1 = int.Parse(Console.ReadLine());
+    int cislo2 = int.Parse(Console.ReadLine());
+    int cislo3 = int.Parse(Console.ReadLine());
+    int objem = objekt.objemObjektu(cislo1, cislo2, cislo3);
+    Console.WriteLine("zde je objem tělesa: " + objem);
+}
+else if (odpoved == "povrch")
+{
+    Objekt objekt = new Objekt();
+    Console.WriteLine("Zadej délky stran");
+    int cislo1 = int.Parse(Console.ReadLine());
+    int cislo2 = int.Parse(Console.ReadLine());
+    int cislo3 = int.Parse(Console.ReadLine());
+    int povrch = objekt.povrchObjektu(cislo1, cislo2, cislo3);
+    Console.WriteLine("zde je povrch tělesa: " + povrch);
+}
+else if (odpoved == "tělesová úhlopříčka")
+{
+    Objekt objekt = new Objekt();
+    Console.WriteLine("zadej délky stran");
+    int cislo1 = int.Parse(Console.ReadLine());
+    int cislo2 = int.Parse(Console.ReadLine());
+    int cislo3 = int.Parse(Console.ReadLine());
+    int uhloprickaT = objekt.telesovaUhlopricka(cislo1, cislo2, cislo3);
+    Console.WriteLine("zde je tělesová úhlopříčka: " + uhloprickaT);
+}
+else
+{
+    Objekt objekt = new Objekt();
+    Console.WriteLine("zadej délky stran");
+    int cislo1 = int.Parse(Console.ReadLine());
+    int cislo2 = int.Parse(Console.ReadLine());
+    int uhloprickaS = objekt.stenovaUhlopricka(cislo1, cislo2);
+    Console.WriteLine("zde je stěnová úhlopříčka: " + uhloprickaS);
+}
